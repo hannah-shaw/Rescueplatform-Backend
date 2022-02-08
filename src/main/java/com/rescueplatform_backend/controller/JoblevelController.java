70fe368cd32398a49d84445d9fd9f69d18280af2
hannah-shaw <xiaohannwork@163.com>
2022-02-08 09:42:33 +0800

    feat:权限组角色+职称增删改查

--- src/main/java/com/rescueplatform_backend/controller/JoblevelController.java
+++ src/main/java/com/rescueplatform_backend/controller/JoblevelController.java
@@ -1,9 +1,16 @@
 package com.rescueplatform_backend.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.rescueplatform_backend.entity.Joblevel;
+import com.rescueplatform_backend.entity.RespBean;
+import com.rescueplatform_backend.service.JoblevelService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * <p>
@@ -13,8 +20,54 @@ import org.springframework.web.bind.annotation.RestController;
  * @author hannah
  * @since 2022-02-03
  */
+@Api(value = "职称管理",tags = "职称管理")
 @RestController
-@RequestMapping("/joblevel")
+@RequestMapping("/system/basic/joblevel")
 public class JoblevelController {
 
+    @Autowired
+    JoblevelService joblevelService;
+
+    @ApiOperation("职称列表")
+    @GetMapping("/list")
+    public List<Joblevel> getAllJoblevel(){
+        List<Joblevel> list = joblevelService.list();
+        return list;
+    }
+
+    @ApiOperation("添加职称")
+    @PostMapping("/add")
+    public RespBean addJoblevel(@RequestBody Joblevel joblevel){
+        joblevel.setCreateDate(LocalDateTime.now());
+        if (joblevelService.save(joblevel)){
+            return RespBean.success("添加成功！");
+        }
+        return RespBean.error("添加失败！");
+    }
+
+    @ApiOperation("修改职称")
+    @PutMapping("/update")
+    public RespBean updateJoblevel(@RequestBody Joblevel joblevel){
+        if (joblevelService.updateById(joblevel)){
+
+            return RespBean.success("更新成功！");
+        }
+        return RespBean.error("更新失败！");
+    }
+
+    @ApiOperation("删除职称")
+    @DeleteMapping("/delete")
+    public RespBean deleteJoblevel(@RequestParam("id")Long id){
+
+        joblevelService.removeById(id);
+        return RespBean.success("删除成功！");
+    }
+
+    @ApiOperation("批量删除职称")
+    @DeleteMapping("/delete/ids")
+    public RespBean deleteByIds(@RequestParam("ids") List<Long> ids){
+        joblevelService.removeByIds(ids);
+
+        return RespBean.success("删除成功");
+    }
 }
---
+++ src/main/java/com/rescueplatform_backend/controller/PermissionController.java
@@ -0,0 +1,82 @@
+package com.rescueplatform_backend.controller;
+
+
+import com.rescueplatform_backend.entity.RespBean;
+import com.rescueplatform_backend.entity.Role;
+import com.rescueplatform_backend.service.MenuRoleService;
+import com.rescueplatform_backend.service.MenuService;
+import com.rescueplatform_backend.service.RoleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @description: 权限组
+ * @author: hannah
+ */
+@Api(value = "权限管理",tags = "权限管理")
+@RestController
+@RequestMapping("/system/basic/permission")
+public class PermissionController {
+
+    @Autowired
+    RoleService roleService;
+    @Autowired
+    MenuService menuService;
+    @Autowired
+    MenuRoleService menuRoleService;
+
+    @ApiOperation("添加角色")
+    @PostMapping("/add")
+    public RespBean addRole(@RequestBody Role role){
+        //判断传入的权限字符是否以 ROLE_ 开头，Security 权限判断要求要 ROLE_ 开头
+        if (!role.getName().startsWith("ROLE_")){
+            //如果不是以 ROLE_ 开头，在添加前拼接一个
+            role.setName("ROLE_" + role.getName());
+        }
+        if (roleService.save(role)){
+
+            return RespBean.success("添加成功！");
+        }
+        return RespBean.error("添加失败！");
+    }
+
+    @ApiOperation("修改角色")
+    @PutMapping("/update")
+    public RespBean update(@RequestBody Role role){
+        if (roleService.updateById(role)){
+            return RespBean.success("修改成功！");
+        }
+        return RespBean.error("修改失败！");
+    }
+
+    @ApiOperation("获取角色列表")
+    @GetMapping("/list")
+    public List<Role> getRoleList(){
+        return roleService.list();
+    }
+
+    @ApiOperation("删除角色")
+    @DeleteMapping("/delete")
+    public RespBean deleteRole(@RequestParam("id") Long id){
+        if (roleService.removeById(id)){
+            return RespBean.success("删除成功！");
+        }
+        return RespBean.error("删除失败！");
+    }
+
+    @ApiOperation("批量删除角色")
+    @DeleteMapping("/delete/ids")
+    public RespBean deleteRoles(@RequestParam("ids") List<Long> ids){
+        if (roleService.removeByIds(ids)){
+            return RespBean.success("删除成功！");
+        }
+        return RespBean.error("删除失败！");
+    }
+
+}
