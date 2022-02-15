5c2397bdcf5206aebcc8966ac7aecb55f72e1f3c
hannah-shaw <xiaohannwork@163.com>
2022-02-15 15:00:42 +0800

    feat:websocket聊天
    未配合前端

---
+++ src/main/java/com/rescueplatform_backend/controller/ChatController.java
@@ -0,0 +1,36 @@
+package com.rescueplatform_backend.controller;
+
+import com.rescueplatform_backend.entity.Admin;
+import com.rescueplatform_backend.service.AdminService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+/**
+ * @program: yeb
+ * @description: 和谁聊天
+ * @author: Honors
+ * @create: 2021-08-02 14:12
+ */
+@RestController
+@RequestMapping("/chat")
+@Api(value = "在线聊天",tags = "在线聊天")
+public class ChatController {
+    @Autowired
+    private AdminService AdminService;
+
+    @ApiOperation(value = "获取所有操作员")
+    @GetMapping("/admin")
+    public List<Admin> getAllAdmins(String keywords){
+        //可以搜索和谁聊天
+        return AdminService.getAllAdmins(keywords);
+    }
+
+
+}
