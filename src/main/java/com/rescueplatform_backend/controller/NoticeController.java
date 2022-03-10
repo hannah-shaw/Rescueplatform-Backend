5fbd0237fb7f58e73bb8991512f088cd9b37ed1b
hannah-shaw <xiaohannwork@163.com>
2022-03-10 10:33:21 +0800

    feat:通知管理

--- src/main/java/com/rescueplatform_backend/controller/NoticeController.java
+++ src/main/java/com/rescueplatform_backend/controller/NoticeController.java
@@ -1,20 +1,81 @@
 package com.rescueplatform_backend.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.rescueplatform_backend.entity.Notice;
+import com.rescueplatform_backend.entity.RespBean;
+import com.rescueplatform_backend.service.NoticeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ *  通知管理
  * </p>
  *
  * @author hannah
  * @since 2022-02-03
  */
+@Api(value = "通知管理",tags = "通知管理")
 @RestController
-@RequestMapping("/notice")
+@RequestMapping("/front/notice")
 public class NoticeController {
+    @Autowired
+    private NoticeService noticeService;
 
+    @ApiOperation(value = "获取所有通知信息")
+    @GetMapping("/list")
+    public List<Notice> getAllNotice(){
+        return noticeService.list();
+    }
+
+    @ApiOperation(value = "获取最新通知信息")
+    @GetMapping("/newNotice")
+    public Notice getNewNotice(){
+        List<Notice> list = noticeService.list();
+        return list.get(list.size() - 1);
+    }
+
+    @ApiOperation(value = "添加通知信息")
+    @PostMapping("/add")
+    public RespBean addPosition(@RequestBody Notice notice){
+        notice.setCreatetime(LocalDateTime.now());
+        if (noticeService.save(notice)){
+            return RespBean.success("添加成功！");
+        }
+        return RespBean.error("添加失败！");
+    }
+
+    @ApiOperation(value = "更新通知信息")
+    @PutMapping("/update")
+    public RespBean updatePosition(@RequestBody Notice notice){
+        if (noticeService.updateById(notice)){
+            return RespBean.success("更新成功!");
+        }
+        return RespBean.error("更新失败！");
+    }
+
+    @ApiOperation(value = "删除通知信息")
+    @DeleteMapping("/delete")
+    public RespBean deletePosition(@RequestParam("id") Integer id){
+        if (noticeService.removeById(id)){
+            return RespBean.success("删除成功！");
+        }
+        return RespBean.error("删除失败!");
+    }
+
+    @ApiOperation(value = "批量删除通知信息")
+    @DeleteMapping("/deletes")
+    public RespBean deletePositionByIds(@RequestParam Integer[] ids){
+        System.out.println("删除岗位信息");
+        if (noticeService.removeByIds(Arrays.asList(ids))){
+            return RespBean.success("删除成功！");
+        }
+        return RespBean.error("删除失败!");
+    }
 }
--- src/main/java/com/rescueplatform_backend/entity/Notice.java
+++ src/main/java/com/rescueplatform_backend/entity/Notice.java
@@ -1,5 +1,7 @@
 package com.rescueplatform_backend.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.time.LocalDateTime;
 import java.io.Serializable;
@@ -26,12 +28,16 @@ public class Notice implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
+    @ApiModelProperty(value = "通知标题")
     private String title;
 
+    @ApiModelProperty(value = "管理员id")
     private String adminid;
 
+    @ApiModelProperty(value = "发布时间")
     private LocalDateTime createtime;
 
 
